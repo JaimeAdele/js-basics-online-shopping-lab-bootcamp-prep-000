ea66b2aef83554390ebaca112637e5a49362285f
Jaime Zuspann <jaimeadele@gmail.com>
2017-10-18 16:52:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined){
     console.log('Sorry, we don\'t have a credit card on file for you.');
   } else {
-    console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+    const cartTotal = total();
+    console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   }
 }
